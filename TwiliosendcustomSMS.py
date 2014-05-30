c87e05ce2d54bb012cab9e54571af941d9fad848
ahoughstow <amber.houghstow@gmail.com>
2014-05-30 03:33:30 -0400

    protecting auth token

--- TwiliosendcustomSMS.py
+++ TwiliosendcustomSMS.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 from twilio.rest import TwilioRestClient
 
+from local_settings import *
+
 app = Flask(__name__)
-account = "ACf65d7f6a52e562109340437ab8dc2829"
-token = "9c7237a15b4c3bdc49574898232e79d5"
 client = TwilioRestClient(account, token)
 
 @app.route('/')
