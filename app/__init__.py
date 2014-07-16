37b9bac3e183ed095c6131d8c354b7f018774ffd
ahoughstow <amber.houghstow@gmail.com>
2014-07-15 23:17:47 -0400

    debug

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+print "step 1"
 app = Flask(__name__)
+print "step 2"
 app.config.from_object('config')
+print "step 3"
 db = SQLAlchemy(app)
+print "step 4"
 
 from app import views, models
+print "step 5"
 
 
