d89a366d74d8913fc07cad5de88e56d96ca83256
ahoughstow <amber.houghstow@gmail.com>
2014-11-21 15:08:19 -0500

    added a default sqlite config

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+if 'DATABASE_URL' in os.environ:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+else:
+    # defaults to a sqllite database named data.db
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'data.db')
+    print "WARNING: using default sqlite database"
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
+
+print SQLALCHEMY_DATABASE_URI
+
